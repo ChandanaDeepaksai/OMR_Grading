84c5885a6ab53d7b1e9134d10557cc892806873d
ChandanaDeepaksai <128075078+ChandanaDeepaksai@users.noreply.github.com>
2024-10-09 11:47:48 +0530

    Update utlis.py

--- utlis.py
+++ utlis.py
@@ -30,7 +30,7 @@ def stackImages(imgArray,scale,lables=[]):
     if len(lables) != 0:
         eachImgWidth= int(ver.shape[1] / cols)
         eachImgHeight = int(ver.shape[0] / rows)
-        print(eachImgHeight)
+        #print(eachImgHeight)
         for d in range(0, rows):
             for c in range (0,cols):
                 cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
@@ -42,7 +42,7 @@ def rectCountour(coutours):  #detects small contours accourding to aera
     rectCon=[]
     for i in coutours:
         area = cv2.contourArea(i)
-        print("Area",area)
+        # print("Area",area)
         if area>50:
             peri = cv2.arcLength(i,True)
             approx = cv2.approxPolyDP(i,0.02*peri,True)
